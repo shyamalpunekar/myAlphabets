8457959f328628fb31c6db5ffd3526ddbd3a33d5
shy <shyamal.punekar@gmail.com>
2017-10-11 14:25:12 -0700

    Add letters

---
+++ app/src/main/java/com/epicodus/myalphabets/MainActivity.java
@@ -0,0 +1,20 @@
+package com.epicodus.myalphabets;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
+    String[] letters = new String[]{
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+            "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
